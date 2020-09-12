f0d9f98d1c86b4a9e611967ce7720555eac08159
Sky <vivek22122014@gmail.com>
2020-09-12 23:24:32 +0530

    Jarvis Part 1  Speech to Text Python Project - #Automated

---
+++ Jarvis Part 1  Speech to Text Python Project - #Automated.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+import sys
+
+v=sr.Recognizer()
+
+while(1):
+    with sr.Microphone() as source:
+        print('Jarvis : Speak Anything Sir')
+        audio=v.listen(source)
+
+        try:
+            text=v.recognize_google(audio)
+            print('Vivek said : ', format(text))
+
+        except:
+            print('Sorry Sir I did not get that')
+
+        if(format(text)=='exit'):
+            sys.exit('Have a nice day sir')
